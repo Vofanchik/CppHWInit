58057549c738c59e57c613366981bd5524f60267
Vladimir Kalinin <vofanchic@mail.ru>
2023-01-08 23:51:20 +0500

    ceaser2_done

--- calc.cpp
+++ calc.cpp
@@ -9,24 +9,24 @@ int main()
     cout << "для шифрования - 1, дешифрования - 2, остальное для выхода" << endl;
     int sel = 0;
     cin >> sel;
-    char *greet = new char[];
-    cin >> greet;
-    cout << greet;
+
     while (sel == 1 || sel == 2)
     {
-        int size;
+        int size = 0;
         cout << "введите размер массива" << endl;
         cin >> size;
         int shift;
         cout << "введите размер сдвига" << endl;
         cin >> shift;
-        char *array = new char[size];
-        cout << "введите символы поочерёдно" << endl;
+        char *array = new char[100];
+        cout << "введите символы " << endl;
+        cin >> array;
 
-        for (int i = 0; i < size; ++i)
-        {
-            cin >> array[i]; 
-        }
+        //for (int i = 0; i < size; ++i)
+        //{
+        //    cin >> array[i]; 
+        //}
+
         
         if (sel == 1)
         {
